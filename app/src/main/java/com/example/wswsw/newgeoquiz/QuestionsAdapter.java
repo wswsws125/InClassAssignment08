3bf7a84ca501c0b10042995abb52260fb95254c2
wswsws125 <43149266+wswsws125@users.noreply.github.com>
2019-03-18 22:10:47 -0400

    Update QuestionsAdapter.java

--- app/src/main/java/com/example/wswsw/newgeoquiz/QuestionsAdapter.java
+++ app/src/main/java/com/example/wswsw/newgeoquiz/QuestionsAdapter.java
@@ -13,23 +13,28 @@ public class QuestionsAdapter extends RecyclerView.Adapter<QuestionsViewHolder> 
     private List<Question> questions;
     private Context context;
 
+    //Adapter: to show the questions on the recyclerView
     public QuestionsAdapter(List<Question> questions, Context context){
         this.questions= questions;
         this.context=context;
     }
+
+    //Using ViewHolder: to improve operation efficiency
     @Override
     public QuestionsViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
         View view =LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view_country, parent,false);
         return new QuestionsViewHolder(view,context);
     }
 
+    //Each child holder is bound to the corresponding data
     @Override
     public void onBindViewHolder(QuestionsViewHolder holder, int position){
         Question question=questions.get(position);
         holder.questionCountry.setText(question.location);
         holder.countryQuestion.setText(question.question);
         holder.countryImage.setImageResource(question.photoId);
-        holder.isTrue.setText(Boolean.toString(question.isTrue));
+        holder.isTrue=question.isTrue;
+        //holder.isTrue.setText(Boolean.toString(question.isTrue));
     }
 
     @Override
